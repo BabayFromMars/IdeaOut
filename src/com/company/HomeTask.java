0d3f91d0a62b3caebb80623914eceaa26db55d18
BabayFromMars <xxkotbaunxx@gmail.com>
2018-04-29 12:56:47 +0300

    HomeTask1. Completed.

---
+++ src/com/company/HomeTask.java
@@ -0,0 +1,193 @@
+package com.company;
+import java.util.Scanner;
+
+public class HomeTask {
+    public static void main(String[] args) {
+
+
+                //Домашнее задание 1. Литвинов Е.
+
+                //6.Ввести с консоли n целых чисел. На консоль вывести:
+                //6.1. Четные и нечетные числа.
+                //6.2. Наибольшее и наименьшее число.
+                //6.3. Числа, которые делятся на 3 или на 9.
+                //6.4. Числа, которые делятся на 5 и на 7.
+                //6.5. Все трехзначные числа, в десятичной записи которых нет одинаковых
+                //цифр.
+                //6.6. «Счастливые» числа.
+
+
+       /* System.out.println("Сколько чисел вы хотите ввести: ");
+        Scanner scan = new Scanner(System.in);
+        int amount = scan.nextInt();
+        int[] myArray = new int[amount];
+
+
+        for (int i = 0; i < amount; i++) {
+            System.out.println("Введите целое число: ");
+            myArray[i] = scan.nextInt();
+
+        }
+        for (int i = 0; i < amount; i++) {
+            if (myArray[i] % 2 == 0) {
+                System.out.println(myArray[i] + " - четное число; ");
+            } else System.out.println(myArray[i] + " - нечетное число; ");
+        }
+
+        int min = myArray[0];
+
+        for (int i = 1; i < myArray.length; i++) {
+            if (min > myArray[i]) {
+                min = myArray[i];
+            }
+
+        }
+        System.out.println("Минимальное значение: " + min);
+
+
+        int max = myArray[0];
+
+        for (int i = 1; i < myArray.length; i++) {
+            if (max < myArray[i]) {
+                max = myArray[i];
+            }
+
+        }
+        System.out.println("Максимальное значение: " + max);
+
+
+        for (int i = 0; i < amount; i++) {
+            if (myArray[i] % 3 == 0 || (myArray[i] % 9 == 0)) {
+                System.out.println(myArray[i] + " - делится на 3 или на 9; ");
+            } else System.out.print("");
+        }
+
+        for (int i = 0; i < amount; i++) {
+            if (myArray[i] % 5 == 0 && (myArray[i] % 7 == 0)) {
+                System.out.println(myArray[i] + " - делится на 5 и на 7; ");
+            } else System.out.print("");
+        }
+
+        String[] num = new String[myArray.length];
+        for (int i = 0; i < myArray.length; i++) {
+            num[i] = String.valueOf(myArray[i]);
+            if (num[i].length() == 3) {
+                String numbers = num[i];
+                if (numbers.charAt(0) != numbers.charAt(1) && numbers.charAt(0) != numbers.charAt(2) &&
+                        numbers.charAt(1) != numbers.charAt(2))
+                    System.out.println("Трехзначное число в котором нет одинаковых цифр: " + num[i]);
+            }
+
+
+        }
+
+        for (int i = 0; i < myArray.length; i++) {
+
+            int polnoeChislo = myArray[i];
+
+
+            String str = String.valueOf(polnoeChislo);
+
+
+            if (str.length() > 3 && str.length() % 2 == 0) {
+
+
+                String firstPart = str.substring(0, str.length() / 2);
+                String secondPart = str.substring(str.length() / 2, str.length());
+
+                int pervayaPolovina = Integer.parseInt(firstPart);
+                int vtorayayaPolovina = Integer.parseInt(secondPart);
+                int zapasnoe1 = pervayaPolovina;
+                int sum1 = 0;
+                while (zapasnoe1 != 0) {
+                    sum1 += zapasnoe1 % 10;
+                    zapasnoe1 /= 10;
+
+                    int zapasnoe2 = vtorayayaPolovina;
+                    int sum2 = 0;
+                    while (zapasnoe2 != 0) {
+                        sum2 += zapasnoe2 % 10;
+                        zapasnoe2 /= 10;
+                        if (sum1 == sum2) {
+
+                        }
+                    }
+                }
+
+                System.out.println(firstPart + secondPart + " - счастливое число;");
+            }
+
+        }*/
+
+
+//5. Ввести целые числа как аргументы командной строки,
+// подсчитать их суммы (произведения) и вывести результат на консоль.
+
+/*        int number1, number2;
+        System.out.println("Please enter first number: ");
+        Scanner scan1 = new Scanner(System.in);
+        number1 = scan1.nextInt();
+        System.out.println("Please enter first number: ");
+        Scanner scan2 = new Scanner(System.in);
+        number2 = scan2.nextInt();
+        int sum = number1 + number2;
+        System.out.println("Summ of numbers = " + sum);
+        System.out.println("Product of numbers = " + number1 * number2);*/
+
+//4. Ввести пароль из командной строки и сравнить его со строкой-образцом.\
+
+   /*     String password = "qwerty";
+
+        String str;
+        do {
+            System.out.println("Please enter your password: ");
+            Scanner scan = new Scanner(System.in);
+            str = scan.nextLine();
+            if (!str.equals(password))
+                System.out.println("Password is incorrect.");
+        } while (!str.equals(password));
+        System.out.println("Welcome to the system.");*/
+
+
+//3. Вывести заданное количество случайных чисел
+// с переходом и без перехода на новую строку.
+
+      /*  int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+
+        System.out.println(a + "" + b + "" + c + "" + d + "" + e);
+        System.out.print(a + "\n" + b + "\n" + c + "\n" + d + "\n" + e);*/
+
+
+//2. Отобразить в окне консоли аргументы командной строки в обратном порядке.
+
+    /*    for (int i = args.length - 1; i >= 0; i--) {
+            System.out.println("Argument[" + i + "]: " + args[i]);
+        }*/
+
+
+// 1. Приветствовать любого пользователя посе ввода им имени в консоль.
+
+    /*    System.out.println("Hi, please enter your name: ");
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        System.out.println("Hi, " + str);
+*/
+
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
